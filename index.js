aab3d546f4949eaa9ab8498bbc61e394ace2f1bf
akvam <akvam@statoil.com>
2015-09-21 11:40:40 +0200

    added dynamic port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express()
+var port = (process.env.PORT || '3000')
 
 app.get('/', function (req, res) {
 	res.send('Hello World')
 })
 
-app.listen(3000);
-console.log("I have started please stop me when needed");
+app.listen(port);
+console.log('Server startet on port ' + port);
