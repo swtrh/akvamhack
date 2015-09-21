8d12055f84fb01c242ab61dbff3f2f5c66830bc4
akvam <akvam@statoil.com>
2015-09-21 14:40:46 +0200

    Added a form

--- routes/store.js
+++ routes/store.js
@@ -4,7 +4,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('store', { title: 'Express in Store' });
+  res.render('store', { title: 'Express in Store', messageURL: process.env.MESSAGE_URL });
+});
+
+router.route('/message')
+  .post(function (req, res, next) {
+   console.log('Receive message ' + req.body.message);
+   res.send('Message was ' + req.body.message);
 });
 
 module.exports = router;
