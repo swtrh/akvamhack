4ed9b61d4816ff8f4b4503703eeed6ebe9ccff49
akvam <akvam@statoil.com>
2015-09-21 13:46:23 +0200

    addes store url

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+//router.get('/store', function(req, res, next) {
+//  res.render('store', { title: 'Express in Store' });
+//});
+
 module.exports = router;
